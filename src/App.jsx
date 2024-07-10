44b7477eaf99a63bea2bae4e0fad29c3ba0c383d
Rahul22007896 <rahulict518@gmail.com>
2024-07-10 09:51:09 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import './App.css';
+
+const App = () => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const toggleDetails = () => {
+    setShowDetails(!showDetails);
+  };
+
+  return (
+    <div className="container">
+      <div className="card">
+        <div className="icon">
+        <img src="public/212221043006-RAHUL K-CSE.jpg" alt="eye" />
+        </div>
+        <h2 className="name">Name: Rahul K</h2>
+        <p className="registration">Registration No: 212221043006</p>
+        <button className="button" onClick={toggleDetails}>
+          {showDetails ? 'Hide Details' : 'Show Details'}
+        </button>
+        {showDetails && (
+          <div className="details">
+            Status:Student 
+            Place:Chennai
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default App;
